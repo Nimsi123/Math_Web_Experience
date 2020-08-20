216921c1ee47309a147b5a2c3f0d289e75342b65
Nimsi123 <nimsi@berkeley.edu>
2020-08-19 17:49:52 -0700

    Add script.py to tracked files

--- js/pdf_canvas.js
+++ js/pdf_canvas.js
@@ -17,10 +17,18 @@ var db = firebase.firestore();
 
 
 function genPDF(){
-	db.collection("Addition")
+	db.collection("Rational Number Operations")
 	.get()
 	.then(function(querySnapshot) {
 			var count = 0;
+			var displacement = 0;
+			var displacementFactor = 18;
+
+			var blockCounter = 0;
+
+			var leftLim = 9;
+			var rightLim = 18;
+
 			
 	        querySnapshot.forEach(function(doc) {
 	        	//alert(count);
@@ -29,20 +37,61 @@ function genPDF(){
 
 				//'<div>  <div style= "float:left; left:0%;"><p>1) </p></div> <div style = "width:20%; float:left;"><p>$$\sqrt{19}$$</p></div>  </div><br><br><br><br>'
 				//'<div>  <div style= "float:left; left:0%;"><p>1) </p></div> <div style = "width:20%; float:left;"><p>' +  + '</p></div>  </div><br><br><br><br>'
-
+				//The LaTeX parts need space from one another. Achieve this by adding many line breaks
 				
-				if (count < 9){
-	            	document.getElementById("lqb").innerHTML += '<div>  <div style= "float:left; left:0%;"><p>' + (count+1) + ')' + ' ' + '</p></div> <div style = "width:80%; float:left;"><p>' + doc.data()["problem"] + '</p></div>  </div><br><br><br><br>';
-	            	document.getElementById("lab").innerHTML += '<div>  <div style= "float:left; left:0%;"><p>' + (count+1) + ')' + ' ' + '</p></div> <div style = "width:80%; float:left;"><p>' + doc.data()["answer"] + '</p></div>  </div><br><br><br><br>';
+				//if ((count + displacement) > 30){alert(count+displacement);}
+				
+				
+
+				if (count < leftLim){
+					//alert("lqb" + (Math.floor((count+displacement)/18)));
+	            	document.getElementById("lqb" + blockCounter).innerHTML += '<div>  <div style= "float:left; left:0%;"><p>' + (count+displacement+1) + ')' + ' ' + '</p></div> <div style = "width:80%; float:left;"><p>' + doc.data()["problem"] + '</p></div>  </div><br><br><br><br>';
+	            	document.getElementById("lab" + blockCounter).innerHTML += '<div>  <div style= "float:left; left:0%;"><p>' + (count+displacement+1) + ')' + ' ' + '</p></div> <div style = "width:80%; float:left;"><p>' + doc.data()["answer"] + '</p></div>  </div><br><br><br><br>';
 	            	}
 
-	            else if (count < 18){
-	            	document.getElementById("rqb").innerHTML += '<div>  <div style= "float:left; left:0%;"><p>' + (count+1) + ')' + ' ' + '</p></div> <div style = "width:80%; float:left;"><p>' + doc.data()["problem"] + '</p></div>  </div><br><br><br><br>';
-	            	document.getElementById("rab").innerHTML += '<div>  <div style= "float:left; left:0%;"><p>' + (count+1) + ')' + ' ' + '</p></div> <div style = "width:80%; float:left;"><p>' + doc.data()["answer"] + '</p></div>  </div><br><br><br><br>';
+	            else if (count < rightLim){
+	            	//alert("rqb" + (Math.floor((count+displacement)/18)));
+	            	document.getElementById("rqb" + blockCounter).innerHTML += '<div>  <div style= "float:left; left:0%;"><p>' + (count+displacement+1) + ')' + ' ' + '</p></div> <div style = "width:80%; float:left;"><p>' + doc.data()["problem"] + '</p></div>  </div><br><br><br><br>';
+	            	document.getElementById("rab" + blockCounter).innerHTML += '<div>  <div style= "float:left; left:0%;"><p>' + (count+displacement+1) + ')' + ' ' + '</p></div> <div style = "width:80%; float:left;"><p>' + doc.data()["answer"] + '</p></div>  </div><br><br><br><br>';
 	            	}
 	            
-	            else if (count >= 18){
-	            	return;
+	            else if (count >= rightLim){
+	            	//make a new page
+	            	//	add div blocks for questions and answers
+	            	//alert("alo");
+	            	blockCounter+= 1;
+	            	
+	            	document.getElementById("question_block").innerHTML += `		
+	            	<div id = "page">		
+			            <div id = ` + ("lqb" + blockCounter) + ` style = "float:left; width:50%;">
+						</div>
+
+						<div id = ` + ("rqb" + blockCounter) + ` style = "float:left; width:50%;">
+						</div>
+
+						<footer>Footer</footer>
+					</div>
+					`;
+
+					document.getElementById("answer_block").innerHTML += `			
+					<div id = "page">	
+			            <div id = ` + ("lab" + blockCounter) + ` style = "float:left; width:50%;">
+						</div>
+
+						<div id = ` + ("rab" + blockCounter) + ` style = "float:left; width:50%;">
+						</div>
+
+						<footer>Footer</footer>
+					</div>
+					`;
+
+	            	count = -1; //to offset the upcoming count += 1;
+	            	displacement += displacementFactor;
+
+	            	//with the info part out of the way, we can fit more questions into the page!
+	            	leftLim = 13;
+	            	rightLim = 26;
+	            	displacementFactor = 26;
 	            }
 	            
 
---
+++ script.py
@@ -0,0 +1,31 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+from Math_Program.Algorithms_Verification import Algebra1_A_to_C
+
+class FirebasePlugIn:
+	def __init__(self):
+		cred = credentials.Certificate(r"C:\Users\nimar\Downloads\math-program-firebase-adminsdk-z8z8v-2219b9386f (1).json")
+		firebase_admin.initialize_app(cred)
+
+		self.db = firestore.client()
+
+	def addQuestion(self, classNombre, skillNombre, pregunta):
+		# Add a new doc in collection 'Class' with ID 'Skill'
+		self.db.collection(classNombre).document(skillNombre).set(pregunta)
+
+	def addAlgorithm(self, generatorInstance, skillNombre, numeroDePreguntas):
+		for i in range(numeroDePreguntas):
+			package = generatorInstance()
+			data = {"number": str(i+1), "problem": package[0], "answer": package[1]}
+			#self.db.collection(skillNombre).document(str(i+1)).set(data)
+			self.db.collection(skillNombre).document().set(data)
+
+#genList = [Algorithms.Addition(100, 800), Algorithms.Subtraction(100, 800), Algorithms.MultDivRules(-40, 40), 
+#			Algorithms.Multiplication(7, 12), Algorithms.Division(2, 12)]
+
+genList = [Algebra1_A_to_C.RationalNumberOperations(-12, 12, "+")]
+
+questionDump = FirebasePlugIn()
+questionDump.addAlgorithm(genList[0], genList[0].name, 40)
