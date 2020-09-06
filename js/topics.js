8889529ee5fe3acc7657893f515474f6d574face
Daniel Etaat <34327451+DanielEtat@users.noreply.github.com>
2020-09-06 13:58:27 -0700

    First draft of website.

---
+++ js/topics.js
@@ -0,0 +1,26 @@
+var subject;
+
+window.onload = function() {
+  console.log("getting arguments.");
+  var params = getParams(window.location.href);
+  subject = params["subject"];
+  addSubject();
+}
+
+var getParams = function(url) {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
+
+var addSubject = function() {
+  var element = document.getElementById("subject");
+  element.innerText = subject;
+}
